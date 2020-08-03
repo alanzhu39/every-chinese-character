f1b0fbf87b4abab23dc322e99c9ce70456ecdd1c
alanzhu39 <36008118+alanzhu39@users.noreply.github.com>
2020-08-02 22:20:37 -0700

    testing gitignore

--- main.py
+++ main.py
@@ -5,25 +5,30 @@ import json
 
 cfile = open('characters/characters.txt', 'r', encoding='utf-8')
 clist = json.loads(cfile.read())
+credentials = open('credentials.txt', 'r')
 
 interval = 60 * 60 * 6
 interval = 10
 
-consumer_key = 
-consumer_secret =
-access_key =
-access_secret =
-
+consumer_key = credentials.readline().strip('\n')
+consumer_secret = credentials.readline().strip('\n')
+access_key = credentials.readline().strip('\n')
+access_secret = credentials.readline().strip('\n')
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
 
+api.update_status(clist[0])
+
+"""
 for i in range(len(clist)):
     print('getting character {}'.format(i + 1))
     c = clist[i]
     message = c + " " + pinyin.getPinyin(c)
     api.update_status(message)
     time.sleep(interval)
+"""
 
 cfile.close()
+credentials.close()
